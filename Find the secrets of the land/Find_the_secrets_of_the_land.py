c4b4bd256b1d5f2f2ee768d996114fca4c42f9a3
Addison <ninjasmosa@vivaldi.net>
2022-12-15 18:26:45 +0000

    Bar section complete

--- Find the secrets of the land/Find_the_secrets_of_the_land.py
+++ Find the secrets of the land/Find_the_secrets_of_the_land.py
@@ -1,16 +1,18 @@
-import os
 import time
 import random
 
 # Initialise variables
-playerName = ""
-playerSpecies = ""
-playerHealth = 1
+name = ""
+species = ""
+health = 1
 strength = 1
 money = 100
-gun = True
 ammo = 0
 bear = True
+talkedToDrunkard = False
+
+# Initialise inventory
+inventory = ["Gun"]
 
 def RollDice(side): # Used for dice roll segments
     result = random.randint(1,side)
@@ -36,21 +38,104 @@ def CreateCharacter(): # Create the player character
     time.sleep(1)
     print("")
 
+# Using inventory items
+def HealthKitUse():
+    global inventory
+    global health
+    if "HealthKit" in inventory:
+        print("You used a health kit.\nYou gain 30 health.")
+        health += 30
+    else:
+        print("You don't have a health kit.")
+
+def Bar(): # Enter the bar
+    global money
+    global health
+    global talkedToDrunkard
+    print("""
+You enter the bar. A bartender greets you and offers you a pint of beer.
+The beer costs 10 Money.""")
+    if money >= 10:
+        buybeer = input("Will you buy this beer? 'Yes' or 'no'?").lower()
+        if buybeer == "yes" or buybeer == "n":
+            money -= 10
+            health += 5
+            print("")
+            print("That beer tasted nice. You gain 5 health.")
+        elif buybeer == "no" or buybeer == "n":
+            print("")
+            print("You decide not to buy this beer.")
+        else:
+            print("""I didn't understand that
+            """)
+            bar()
+    else:
+        print("Unfortunately you don't have enough money.")
+    print()
+    print("The bar is largely empty, except for a drunkard sitting at one of the tables.\n")
+    time.sleep(1)
+    if not talkedToDrunkard:
+        choice = input("Do you want to talk to him? 'Yes' or 'No'?").lower()
+        if input == "yes":
+            print("""
+    You walk up to the drunkard. You sit down next to him and he starts a conversation.\n""")
+            print("""'You look like you've come from very far away. In any case, nothing like a good beer to get yourself refreshed.
+    I heard that there is some demonic king out there who is locking people who he deems 'inadequate'.'""")
+            EnterContinue()
+            print("'Wait, from what you told me, it seems like you were victim to his scheme. That must suck.'")
+            EnterContinue()
+            print("'Look. I'm just a mere drunkard so I can't help much, but take some ammo for your gun. And this health kit. I hope this will be useful to you.'")
+            ammo += 16
+            inventory.append("HealthKit")
+            print("You acquire a health kit.")
+            time.sleep(1)
+            print("You decide to leave the bar.\n")
+            time.sleep(2)
+        else:
+            print()
+            print("You decide not to talk to the drunkard and leave the bar.")
+            WestOfStart()
+    else:
+        print("But you don't want to disturb him again, so you leave the bar.\n")
+        time.sleep(1)
+        WestOfStart()
+
+def WestOfStart(): # You head west and the bear is dead
+    choice = input("Where do you want to go? The 'bar', the 'Weapon Store', the 'restaurant', or the house belonging to the 'Mayor'? ").lower()
+    if choice == "bar":
+        bar()
+
+    elif choice == "restaurant":
+        restaurant()
+
+    elif choice == "mayor":
+        mayor()
+
+    elif choice == "weapon store":
+        weaponstore()
+
+    else:
+        print("""I didn't understand that.
+""")
+        westOfStart()
+
 def StartVillage(): # The first part of the game
     global bear
     global ammo
+    global inventory
+    shootBear = ""
     print("There are three roads, each heading in different directions.")
     choice = input("Do you head 'North', 'East', or 'West'? ").lower()
     if choice == "west": # Go west of the start position
         print("")
         print("You head west. A few markets can be found along with an exit gate.")
         time.sleep(1)
-        if bear:
+        if bear: #The bear is not dead
             print("However, there is a bear standing in the way. It looks strong and dangerous. It could kill you easily.")
             time.sleep(1)
-            if ammo != 0:
-                while shootBear != yes or shootBear != no:
-                    shootBear = input("Will you shoot the bear? 'Yes' or 'No'").lower
+            if ammo != 0 and "Gun" in inventory:
+                while shootBear != "yes" or shootBear != "no":
+                    shootBear = input("Will you shoot the bear? 'Yes' or 'No'").lower()
                     if shootBear == "yes":
                         ammo -= 1
                         bear = False
@@ -66,7 +151,7 @@ def StartVillage(): # The first part of the game
                 time.sleep(1)
                 print("")
                 StartVillage()
-        else:
+        else: # The bear is dead
             westOfStart()
 
     elif choice == "east":
@@ -78,21 +163,6 @@ def StartVillage(): # The first part of the game
     else:
         print("I didn't understand that.")
         StartVillage()
-
-def westOfStart():
-    choice = input("Where do you want to go? The 'bar', the 'Weapon Store', the 'restaurant', or the house belonging to the 'Mayor'? ").lower()
-    if choice == "bar":
-
-    elif choice == "restaurant":
-
-    elif choice == "mayor":
-
-    elif choice == "weapon store":
-
-    else:
-        print("""I didn't understand that.
-""")
-        westOfStart()
 
 # Starting text
 def begin():
